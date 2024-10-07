Add .gitignore to ignore checkpoints directory Fix image normalization in DepthAnythingV2

depth_anything_v2/dpt.py
@@ -211,7 +211,7 @@ class DepthAnythingV2(nn.Module):
         
         h, w = raw_image.shape[:2]
         
-        image = image / 255.0
+        image = raw_image / 255.0
         # image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
         
         image = transform({'image': image})['image']